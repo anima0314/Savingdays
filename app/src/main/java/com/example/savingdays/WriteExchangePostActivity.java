9a592cde83b08734a7d5d93a64776675f4927fa7
anima0314 <82082606+anima0314@users.noreply.github.com>
2021-05-27 17:34:46 +0900

    게시판 2개로 확장했음

---
+++ app/src/main/java/com/example/savingdays/WriteExchangePostActivity.java
@@ -0,0 +1,83 @@
+package com.example.savingdays;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+
+
+public class WriteExchangePostActivity extends AppCompatActivity {
+    private static String TAG = "WriteExchangePostActivity";
+    private FirebaseUser user;
+    private FirebaseFirestore firebaseFirestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_post);
+
+        findViewById(R.id.check).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.check:
+                    profileUpdate();
+                    break;
+            }
+
+        }
+    };
+
+    //10강 20분에서 시작
+    private void profileUpdate(){
+        final String title = ((EditText)findViewById(R.id.titleEditText)).getText().toString();
+        final String contents = ((EditText)findViewById(R.id.contentsEditText)).getText().toString();
+
+        if (title.length() > 0 && contents.length() > 0) {
+            user = FirebaseAuth.getInstance().getCurrentUser();
+
+            PostInfo postInfo = new PostInfo(title, contents, user.getUid(), new Date());
+            uploader(postInfo);
+
+        }else{
+            startToast("게시글을 입력해 주세요.");
+        }
+    }
+    private void uploader(PostInfo postInfo){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("exchange_posts").add(postInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        startToast("게시글을 올렸습니다.");
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    private void startToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
